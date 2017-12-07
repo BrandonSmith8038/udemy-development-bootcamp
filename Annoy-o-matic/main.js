6962a0a8c58d3511e4b9b67eb08c6abed0c1fe59
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-07 09:48:15 -0700

    Ran Prettier

--- Annoy-o-matic/main.js
+++ Annoy-o-matic/main.js
@@ -1,12 +1,12 @@
-const answer1 = 'yes'
-const answer2 = 'yeah'
-const question = 'Are we there yet?'
-const madeIt = 'Yay, we finally made it!'
+const answer1 = 'yes';
+const answer2 = 'yeah';
+const question = 'Are we there yet?';
+const madeIt = 'Yay, we finally made it!';
 
-let userAnswer = prompt(question)
+let userAnswer = prompt(question);
 
-while(!userAnswer.includes(answer1) && !userAnswer.includes(answer2)) {
-    userAnswer = prompt(question)
+while (!userAnswer.includes(answer1) && !userAnswer.includes(answer2)) {
+  userAnswer = prompt(question);
 }
 
-alert(madeIt)
+alert(madeIt);
--- movieDB/main.js
+++ movieDB/main.js
@@ -1,30 +1,34 @@
-const movies = [{
+const movies = [
+  {
     name: 'Walk The Line',
     rating: 9.2,
     haveSeen: true
-}, {
+  },
+  {
     name: 'Dale',
     rating: 8.7,
     haveSeen: false
-},{
+  },
+  {
     name: 'Deadpool',
     rating: 4.7,
     haveSeen: true
-},{
+  },
+  {
     name: 'Titanic',
     rating: 7.8,
     haveSeen: false
-}]
-
+  }
+];
 
 const printMovies = arr => {
-    arr.forEach((el) => {
-        if(el.haveSeen){
-            console.log(`You have seen "${el.name}" - ${el.rating} stars`)
-        } else {
-            console.log(`You have not seen "${el.name}" - ${el.rating} stars`)
-        }
-    })
-}
+  arr.forEach(el => {
+    if (el.haveSeen) {
+      console.log(`You have seen "${el.name}" - ${el.rating} stars`);
+    } else {
+      console.log(`You have not seen "${el.name}" - ${el.rating} stars`);
+    }
+  });
+};
 
-printMovies(movies)
+printMovies(movies);
