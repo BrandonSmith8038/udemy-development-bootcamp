2cf10056b56aebd323ceac770d230dcb42ac5f66
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-09 10:50:57 -0700

    Finished Todo List

--- jQueryTodo/assets/js/main.js
+++ jQueryTodo/assets/js/main.js
@@ -1,4 +1,28 @@
 // Check off specific todos by clicking
-$('li').click(function(){
-    $(this).toggleClass('completed')
-})
+$('ul').on('click', 'li', function() {
+  $(this).toggleClass('completed');
+});
+
+//Remove todo when clicking on the X
+
+$('ul').on('click', 'span', function(e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  e.stopPropagation();
+});
+
+$("input[type='text']").on('keydown', function(e) {
+  let keyCode = e.which;
+  if (keyCode === 13) {
+    const todo = $(this).val();
+    $('ul').append(`<li><span><i class="fa fa-trash"></i></span> ${todo}</li>`);
+    $(this).val('');
+  }
+});
+
+$('.fa-plus').on('click', function() {
+  $("input[type = 'text']").fadeToggle();
+});
