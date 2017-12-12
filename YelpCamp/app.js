9b747c4a513e5d4eac585cbf62458e5507dfd194
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-11 23:38:08 -0700

    Initial Styling

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -15,6 +15,42 @@ const campgrounds = [
   {
     name: "Mountian Goat's Rest",
     image: 'https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg'
+  },
+  {
+    name: 'Salmon Creek',
+    image: 'https://farm6.staticflickr.com/5334/9925256586_c06d949b3e.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm5.staticflickr.com/4514/36822417253_1d7f340b3a.jpg'
+  },
+  {
+    name: "Mountian Goat's Rest",
+    image: 'https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg'
+  },
+  {
+    name: 'Salmon Creek',
+    image: 'https://farm6.staticflickr.com/5334/9925256586_c06d949b3e.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm5.staticflickr.com/4514/36822417253_1d7f340b3a.jpg'
+  },
+  {
+    name: "Mountian Goat's Rest",
+    image: 'https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg'
+  },
+  {
+    name: 'Salmon Creek',
+    image: 'https://farm6.staticflickr.com/5334/9925256586_c06d949b3e.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm5.staticflickr.com/4514/36822417253_1d7f340b3a.jpg'
+  },
+  {
+    name: "Mountian Goat's Rest",
+    image: 'https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg'
   }
 ];
 
