70519026ac79351b7486c305cc9548077df31b67
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-12-15 13:39:25 +0000

    Styling the show page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -30,6 +30,9 @@ mongoose
 //SCHEMA SETUP
 
 app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -6,17 +6,17 @@ const data = [
         {
             name: 'Clouds Rest',
             image: 'https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg',
-            description: 'This is the clouds rest campground'
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
             name: 'Desert Mesa',
             image: 'https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg',
-            description: 'This is the Desert Mesa campground'
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
             name: 'Canyon Floor',
             image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg',
-            description: 'This is the Canyon Floor campground'
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
     ]
 const seedDB = () => {
